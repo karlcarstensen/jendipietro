d9ace23d9f75e64a85174a56d40460eb4a30af72
Karl <karl.carstensen@gmail.com>
2023-09-13 21:23:16 -0700

    Fixing footer

--- src/Footer.js
+++ src/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
 
   return (
     <div className="bg-slate-800 pt-10 pb-16 text-slate-600">
-      <p className="text-center">&copy; {year} Jen Beatty Interior Design.</p>
+      <p className="text-center">&copy; {year} Jen DiPietro Design.</p>
     </div>
   );
 }
