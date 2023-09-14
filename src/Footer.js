870624283afeb8efeafe9c9911ec35bf714c0583
Karl <karl.carstensen@gmail.com>
2023-09-13 20:56:23 -0700

    Updates to site september

--- src/Footer.js
+++ src/Footer.js
@@ -3,9 +3,12 @@ import React from 'react'
 import './footer.css'
 
 function Footer() {
+  let year = new Date()
+  year = year.getFullYear()
+
   return (
     <div className="bg-slate-800 pt-10 pb-16 text-slate-600">
-      <p className="text-center">Jen Beatty Interior Design. 964 Richard Lane. Danville, CA 94526</p>
+      <p className="text-center">&copy; {year} Jen Beatty Interior Design.</p>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -5,9 +5,12 @@ import './header.css'
 function Header() {
   return (
     <div className="py-10">
-      <h1 className="pb-4 text-sm md:text-base"><span className='mr-4'>Lighthouse Talent</span>by Jen Beatty</h1>
+      <h1 className="pb-4 text-sm md:text-base"><span>Jen Beatty </span>Interior Design, San Francisco Bay Area, CA</h1>
       <p className='max-w-3xl py-4'>
-        Jen is a Bay Area native. She has spent the last 15+ years working for Tech startups. As a young girl, Jen would spend hours redesigning her bedroom. She started out helping friends design their new apartments and homes and has turned her passion into a business. <a href="mailto:beatty.jennifer@gmail.com">Please email for inquiries</a>.
+        Even as a young girl, Jen would spend hours redesigning her bedroom. From humble beginings helping friends design their apartments and homes, she has channeled her passion into a full-time business.
+      </p>
+      <p>
+        <a href="mailto:beatty.jennifer@gmail.com">Let's discuss your project</a>
       </p>
     </div>
   );
